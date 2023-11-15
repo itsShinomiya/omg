a13b5e6acbf40707407cfcc3a319d6d7baafc0e9
Serotini <eduardoserotini@gmail.com>
2023-11-15 01:27:59 -0300

    Mudo tudo

---
+++ learnjava/Main.java
@@ -0,0 +1,11 @@
+package learnjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String nome = "oloco";
+        System.out.println("Hello World, and hello " + nome);
+
+    }
+}
